1591f04cfccf5dda15c8d7243975a96671f5766b
ngovandong <dongngo2001@gmail.com>
2022-09-18 19:03:33 +0700

    update .gitignore

--- frontend/host/src/app/store/authSlice.js
+++ frontend/host/src/app/store/authSlice.js
@@ -1,27 +1,19 @@
-import
-{
-  createSlice,
-  createAsyncThunk,
-} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "../../api-service/authService";
 
-export const login = createAsyncThunk('auth/login', async (user) =>
-{
-  const { username, password } = user
-  const res = await authService.login(username, password)
-  return res
-})
-export const getUser = createAsyncThunk('auth/getUser', async () =>
-{
-  const res = await authService.getUser()
-  return res
-})
+export const login = createAsyncThunk("auth/login", async (user) => {
+  const { username, password } = user;
+  const res = await authService.login(username, password);
+  return res;
+});
+export const getUser = createAsyncThunk("auth/getUser", async () => {
+  const res = await authService.getUser();
+  return res;
+});
 let tokenString = null;
 try {
-  tokenString = JSON.parse(localStorage.getItem('token'))
-} catch {
-
-}
+  tokenString = JSON.parse(localStorage.getItem("token"));
+} catch {}
 
 const initialState = {
   user: null,
@@ -32,35 +24,30 @@ const userSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setToken: (state, action) =>
-    {
-      state.token = action.payload
-      localStorage.setItem("token", JSON.stringify(action.payload))
+    setToken: (state, action) => {
+      state.token = action.payload;
+      localStorage.setItem("token", JSON.stringify(action.payload));
     },
-    logout: (state) =>
-    {
+    logout: (state) => {
       state.token = null;
       state.user = null;
-      localStorage.setItem("token", null)
-    }
+      localStorage.setItem("token", null);
+    },
   },
-  extraReducers(builder)
-  {
+  extraReducers(builder) {
     builder
-      .addCase(login.fulfilled, (state, action) =>
-      {
-        state.token = action.payload
-        localStorage.setItem("token", JSON.stringify(action.payload))
+      .addCase(login.fulfilled, (state, action) => {
+        state.token = action.payload;
+        localStorage.setItem("token", JSON.stringify(action.payload));
       })
-      .addCase(getUser.fulfilled, (state, action) =>
-      {
-        state.user = action.payload
-      })
-  }
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+      });
+  },
 });
 
-export const selectUser = state => state.auth.user;
-export const selectToken = state => state.auth.token;
+export const selectUser = (state) => state.auth.user;
+export const selectToken = (state) => state.auth.token;
 
 export const { logout, setToken } = userSlice.actions;
 export default userSlice.reducer;
--- frontend/host/src/components/NavBar.js
+++ frontend/host/src/components/NavBar.js
@@ -1,62 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import Divider from '@mui/material/Divider';
-import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
-import { useDispatch } from 'react-redux';
-import { logout } from '../app/store/authSlice';
-import { NavLink, useNavigate } from 'react-router-dom';
-const links = [{ link: "", name: "Tổng quan" }, { link: "accommodation", name: "Chỗ ở" }, { link: "message", name: "Tin Nhắn" }, { link: "profile", name: "Hồ sơ" }]
-const NavBar = () =>
-{
+import React, { useState, useEffect } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
+import { useDispatch } from "react-redux";
+import { logout } from "../app/store/authSlice";
+import { NavLink, useNavigate } from "react-router-dom";
+const links = [
+  { link: "", name: "Tổng quan" },
+  { link: "accommodation", name: "Chỗ ở" },
+  { link: "message", name: "Tin Nhắn" },
+  { link: "profile", name: "Hồ sơ" },
+];
+const NavBar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
   const [anchorElLanguage, setAnchorElLanguage] = useState(null);
-  const [lan, setLan] = useState('vn');
-  const handleOpenNavMenu = (event) =>
-  {
+  const [lan, setLan] = useState("vn");
+  const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) =>
-  {
+  const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
-  const handleOpenLanguage = (event) =>
-  {
+  const handleOpenLanguage = (event) => {
     setAnchorElLanguage(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () =>
-  {
+  const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () =>
-  {
+  const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
 
-  const handleLogout = () =>
-  {
+  const handleLogout = () => {
     dispatch(logout());
-  }
+  };
 
   return (
-    <AppBar position="static" style={{ backgroundColor: '#349b9f' }}>
+    <AppBar position="static" style={{ backgroundColor: "#349b9f" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <FlightTakeoffIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <FlightTakeoffIcon
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+          />
           <Typography
             variant="h6"
             noWrap
@@ -64,18 +64,18 @@ const NavBar = () =>
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             ReadyBooking
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -90,27 +90,32 @@ const NavBar = () =>
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
-              {links.map(link => <MenuItem key={link.link} onClick={handleCloseNavMenu}>
-                <NavLink className='nav-button' to={link.link} >{link.name}</NavLink>
-              </MenuItem>)}
-
+              {links.map((link) => (
+                <MenuItem key={link.link} onClick={handleCloseNavMenu}>
+                  <NavLink className="nav-button" to={link.link}>
+                    {link.name}
+                  </NavLink>
+                </MenuItem>
+              ))}
             </Menu>
           </Box>
-          <FlightTakeoffIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <FlightTakeoffIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
           <Typography
             variant="h5"
             noWrap
@@ -118,23 +123,29 @@ const NavBar = () =>
             href=""
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {links.map(link =>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {links.map((link) => (
               <NavLink
                 key={link.link}
-                className={({ isActive }) => (isActive ? "nav-item nav-item--active " : "nav-item")}
-                to={link.link} >{link.name}</NavLink>)}
+                className={({ isActive }) =>
+                  isActive ? "nav-item nav-item--active " : "nav-item"
+                }
+                to={link.link}
+              >
+                {link.name}
+              </NavLink>
+            ))}
           </Box>
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Language">
@@ -146,75 +157,77 @@ const NavBar = () =>
               anchorEl={anchorElLanguage}
               id="language-menu"
               open={Boolean(anchorElLanguage)}
-              onClose={
-                () => setAnchorElLanguage(null)
-              }
-              onClick={
-                () => setAnchorElLanguage(null)
-              }
-              sx={{ mt: '45px' }}
-
+              onClose={() => setAnchorElLanguage(null)}
+              onClick={() => setAnchorElLanguage(null)}
+              sx={{ mt: "45px" }}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
             >
-              <MenuItem onClick={() => setLan('vn')}>
-                <span style={{ marginRight: "6px" }} className="fi fi-vn"></span> Viet Nam
+              <MenuItem onClick={() => setLan("vn")}>
+                <span
+                  style={{ marginRight: "6px" }}
+                  className="fi fi-vn"
+                ></span>{" "}
+                Viet Nam
               </MenuItem>
-              <MenuItem onClick={() => setLan('us')}>
-                <span style={{ marginRight: "6px" }} className="fi fi-us"></span> US
+              <MenuItem onClick={() => setLan("us")}>
+                <span
+                  style={{ marginRight: "6px" }}
+                  className="fi fi-us"
+                ></span>{" "}
+                US
               </MenuItem>
             </Menu>
           </Box>
-          <Divider orientation="vertical" style={{ backgroundColor: "white", width: "2px", height: "45px", marginRight: "16px" }} />
+          <Divider
+            orientation="vertical"
+            style={{
+              backgroundColor: "white",
+              width: "2px",
+              height: "45px",
+              marginRight: "16px",
+            }}
+          />
           <Box sx={{ flexGrow: 0 }}>
-
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
               <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">
-                  Profile
-                </Typography>
+                <Typography textAlign="center">Profile</Typography>
               </MenuItem>
               <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">
-                  Account
-                </Typography>
+                <Typography textAlign="center">Account</Typography>
               </MenuItem>
               <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">
-                  Dashboard
-                </Typography>
+                <Typography textAlign="center">Dashboard</Typography>
               </MenuItem>
               <MenuItem onClick={handleLogout}>
-                <Typography textAlign="center">
-                  Logout
-                </Typography>
+                <Typography textAlign="center">Logout</Typography>
               </MenuItem>
             </Menu>
           </Box>
