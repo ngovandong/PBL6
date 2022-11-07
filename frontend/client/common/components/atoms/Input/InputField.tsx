d29e764893087af7f7b825ee022bb157d5f2d715
Khanh Thy Le Ngoc <81146934+thylnk@users.noreply.github.com>
2022-11-07 11:20:33 +0700

    feat(HB-42): UI sign in (#21)

---
+++ frontend/client/common/components/atoms/Input/InputField.tsx
@@ -0,0 +1,42 @@
+import { primaryColor } from '@constants/styles'
+import { styled, InputBase, InputBaseProps } from '@mui/material'
+import { alpha } from '@mui/system'
+
+const Input = styled(InputBase)(({ theme }) => ({
+  '& .MuiInputBase-input': {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    width: '100%',
+    padding: '10px 12px',
+    marginTop: 0,
+    transition: theme.transitions.create([
+      'border-color',
+      'background-color',
+      'box-shadow',
+    ]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      boxShadow: `${alpha(primaryColor, 0.25)} 0 0 0 0.2rem`,
+      borderColor: '#ced4da',
+    },
+  },
+}))
+
+export const InputField = (props: InputBaseProps) => {
+  return <Input id='email' size='small' {...props} />
+}
