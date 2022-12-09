c08518257a79f9d30e34a9ee434e1789e9a79d74
thylnk <thylengockhanh@gmail.com>
2022-12-09 11:31:32 +0700

    feat: update change profile

--- frontend/client/common/utils/api/user/index.ts
+++ frontend/client/common/utils/api/user/index.ts
@@ -3,4 +3,4 @@ import api from '../axios'
 export const getUserInfor = (userId: string) =>
   api.get(`/user/${userId}/user-info`)
 
-export const editUser = (body: any) => api.put(`/user/change-user-info`, body)
+export const editUser = (body: any) => api.put(`/user/change-user-info`, JSON.stringify(body))
