7853f46bf9af9a8c87c7d542f805e3d9ceb70256
Khanh Thy Le Ngoc <81146934+thylnk@users.noreply.github.com>
2022-12-16 14:44:18 +0700

    hotfix: fix middleware (#60)

--- frontend/client/middleware.ts
+++ frontend/client/middleware.ts
@@ -1,18 +1,25 @@
 import { getSession } from 'next-auth/react'
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(req: NextRequest) {
-  const jwt = req.cookies.get('next-auth.session-token')
+import { withAuth } from 'next-auth/middleware'
 
-  console.log(jwt)
-
-  if (!jwt) {
-    req.nextUrl.pathname = '/sign-in'
-    return NextResponse.redirect(req.nextUrl)
-  }
-  return NextResponse.next()
-}
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    console.log(req.nextauth.token)
+    if (req.nextauth.token) {
+      req.nextUrl.pathname = '/sign-in'
+      return NextResponse.redirect(req.nextUrl)
+    }
+    return NextResponse.next()
+  },
+  {
+    pages: {
+      signIn: '/sign-in',
+    },
+    secret: process.env.NEXTAUTH_SECRET
+  },
+)
 
 export const config = {
   matcher: ['/profile/:path*', '/order-management/:path*'],
